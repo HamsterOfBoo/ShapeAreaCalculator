9f42b88952e4f3ccd2e65782aa97c256783a2924
MaximShabanov <maksim.shabanov@waveaccess.ru>
2024-02-29 18:51:43 +0300

    Sql added

---
+++ selectSql.sql
@@ -0,0 +1,4 @@
+﻿Select p.Name as 'ProductName', c.Name as 'CategoryName'
+from Products p
+         left join ProductCategoryIds pc on p.Id == pc.ProductIds
+         left Join Categories c on pc.CategoryId == c.Id
